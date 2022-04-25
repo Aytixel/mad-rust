84f70da9bdee0c475a73f63cf28c987b7a02c179
Aytixel <aytixel@gmail.com>
2022-04-25 21:13:22 +0200

    Add a utility module

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,17 @@
 slint::include_modules!();
 
-use slint::{Model, Timer, TimerMode};
+use slint::{Timer, TimerMode};
 
 fn main() {
     let ui = MainWindow::new();
-
     let ui_handle = ui.as_weak();
-
     let timer = Timer::default();
+
     timer.start(
         TimerMode::Repeated,
-        std::time::Duration::from_secs(5),
+        std::time::Duration::from_millis(50),
         move || {
-            let ui = ui_handle.unwrap();
-            let app_state_count = ui.get_app_states().row_count() as i32;
-            let mut app_state = ui.get_app_state();
-
-            println!("{}, {}", app_state_count, app_state);
-
-            app_state += 1;
-
-            if app_state == app_state_count {
-                app_state = 0;
-            }
-
-            ui.set_app_state(app_state);
+            let _ui = ui_handle.unwrap();
         },
     );
 
---
+++ util/src/timer.rs
@@ -0,0 +1,27 @@
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+#[derive(Debug)]
+pub struct Timer {
+    pub frame_duration: Duration,
+    pub last_update: Instant,
+}
+
+impl Timer {
+    pub fn new(frame_duration: Duration) -> Self {
+        Self {
+            frame_duration: frame_duration,
+            last_update: Instant::now(),
+        }
+    }
+
+    pub fn wait(&mut self) {
+        let elapsed = self.last_update.elapsed();
+
+        if elapsed < self.frame_duration {
+            sleep(self.frame_duration - elapsed);
+        }
+
+        self.last_update = Instant::now()
+    }
+}
