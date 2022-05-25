ec6d3582d2340342c901f9f86a1ecb63ad49e957
Aytixel <aytixel@gmail.com>
2022-05-25 02:27:38 +0200

    Create build.rs

---
+++ driver/mmo7/build.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+fn main() {
+    copy(Path::new("icon.png"));
+}
+
+fn copy(file: &Path) {
+    fs::copy(
+        Path::join(&env::current_dir().unwrap(), file),
+        Path::new(&env::var("OUT_DIR").unwrap()).join(Path::new("../../../").join(file)),
+    )
+    .unwrap();
+}
