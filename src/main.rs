0ab4babd877a56b62996faa3df2c0ce113e92553
Aytixel <aytixel@gmail.com>
2022-05-29 01:42:18 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,14 @@
 mod window;
 
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use std::thread::{spawn, ThreadId};
 use std::time::Duration;
 
-use util::connection::{command::*, Server};
-use util::thread::{kill_double, DualChannel};
 use util::time::Timer;
-use webrender::webrender_api::*;
-
-struct Driver {
-    device_configuration_descriptor: DeviceConfigurationDescriptor,
-    device_list: DeviceList,
-}
-
-impl Driver {
-    fn new(device_configuration_descriptor: DeviceConfigurationDescriptor) -> Self {
-        Self {
-            device_configuration_descriptor: device_configuration_descriptor,
-            device_list: DeviceList::default(),
-        }
-    }
-}
+use webrender::api::units::*;
+use webrender::api::*;
+use webrender::DebugFlags;
+use winit::platform::run_return::EventLoopExtRunReturn;
 
 fn main() {
-    /*
-    if !kill_double() {
-        let server = Server::new();
-        let server_dualchannel = server.dual_channel;
-        let driver_hashmap_mutex = Arc::new(Mutex::new(HashMap::<ThreadId, Driver>::new()));
-
-        run_connection(server_dualchannel, driver_hashmap_mutex);
-    }
-    */
-
     let mut window_options = window::WindowOptions::new("test", 800, 600, Some("./ui/icon.png"));
     window_options.transparent = true;
 
@@ -43,75 +17,145 @@ fn main() {
         Some(ColorF::from(ColorU::new(33, 33, 33, 250))),
         0,
     );
-    let mut timer = Timer::new(Duration::from_micros(3333));
 
-    window.load_font_file(
-        "OpenSans",
-        "./ui/font/OpenSans.ttf",
-        webrender::webrender_api::units::Au::from_f64_px(32.0),
-    );
+    window.load_font_file("OpenSans", "./ui/font/OpenSans.ttf");
 
-    loop {
-        if window.tick() {
-            break;
-        }
-        timer.wait();
-    }
+    run_ui(&mut window);
 
     window.deinit();
 }
 
-// connection processing
-/*
-fn run_connection(
-    server_dualchannel: DualChannel<(ThreadId, bool, Vec<u8>)>,
-    driver_hashmap_mutex: Arc<Mutex<HashMap<ThreadId, Driver>>>,
-) {
-    spawn(move || {
-        let mut timer = Timer::new(Duration::from_millis(100));
+fn run_ui(window: &mut window::Window) {
+    let mut timer = Timer::new(Duration::from_micros(3333));
+    let font = window.load_font("OpenSans", units::Au::from_f64_px(32.0));
 
-        loop {
-            if let Some((thread_id, is_running, data)) = server_dualchannel.recv() {
-                let mut driver_hashmap = match driver_hashmap_mutex.lock() {
-                    Ok(guard) => guard,
-                    Err(poisoned) => poisoned.into_inner(),
-                };
+    loop {
+        let mut do_exit = false;
 
-                if is_running {
-                    if data.len() > 0 {
-                        match Commands::from(data) {
-                            Commands::DeviceConfigurationDescriptor(
-                                device_configuration_descriptor,
-                            ) => {
-                                // initiate driver data
-                                driver_hashmap.insert(
-                                    thread_id,
-                                    Driver::new(device_configuration_descriptor),
-                                );
-                            }
-                            Commands::DeviceList(device_list) => {
-                                if let Some(driver) = driver_hashmap.get_mut(&thread_id) {
-                                    driver.device_list = device_list;
-
-                                    update_device_list_ui(driver_hashmap_mutex.clone());
-                                }
-                            }
-                            _ => {}
+        window
+            .events_loop
+            .run_return(|global_event, _elwt, control_flow| {
+                *control_flow = winit::event_loop::ControlFlow::Exit;
+                match global_event {
+                    winit::event::Event::WindowEvent { event, .. } => match event {
+                        winit::event::WindowEvent::CloseRequested
+                        | winit::event::WindowEvent::KeyboardInput {
+                            input:
+                                winit::event::KeyboardInput {
+                                    virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => do_exit = true,
+                        winit::event::WindowEvent::KeyboardInput {
+                            input:
+                                winit::event::KeyboardInput {
+                                    state: winit::event::ElementState::Pressed,
+                                    virtual_keycode: Some(winit::event::VirtualKeyCode::P),
+                                    ..
+                                },
+                            ..
+                        } => {
+                            println!("set flags {}", window.name);
+                            window
+                                .api
+                                .send_debug_cmd(DebugCommand::SetFlags(DebugFlags::PROFILER_DBG))
                         }
-                    }
-                } else {
-                    // clearing driver data
-                    driver_hashmap.remove(&thread_id);
-
-                    update_device_list_ui(driver_hashmap_mutex.clone());
+                        _ => {}
+                    },
+                    _ => {}
                 }
-            }
+            });
 
-            timer.wait();
+        if do_exit {
+            break;
         }
-    });
-}
 
-// ui processing
-fn update_device_list_ui(driver_hashmap_mutex: Arc<Mutex<HashMap<ThreadId, Driver>>>) {}
-*/
+        let mut frame_builder = window.build_frame();
+        let builder = &mut frame_builder.builder;
+
+        builder.push_simple_stacking_context(
+            frame_builder.bounds.min(),
+            frame_builder.space_and_clip.spatial_id,
+            PrimitiveFlags::IS_BACKFACE_VISIBLE,
+        );
+
+        builder.push_rect(
+            &CommonItemProperties::new(
+                LayoutRect::new(
+                    LayoutPoint::new(100.0, 200.0),
+                    LayoutSize::new(100.0, 200.0),
+                ),
+                frame_builder.space_and_clip,
+            ),
+            ColorF::new(0.0, 1.0, 0.0, 1.0),
+        );
+
+        let text_bounds =
+            LayoutRect::new(LayoutPoint::new(100.0, 50.0), LayoutSize::new(700.0, 200.0));
+        let glyphs = vec![
+            GlyphInstance {
+                index: 48,
+                point: LayoutPoint::new(100.0, 100.0),
+            },
+            GlyphInstance {
+                index: 68,
+                point: LayoutPoint::new(150.0, 100.0),
+            },
+            GlyphInstance {
+                index: 80,
+                point: LayoutPoint::new(200.0, 100.0),
+            },
+            GlyphInstance {
+                index: 82,
+                point: LayoutPoint::new(250.0, 100.0),
+            },
+            GlyphInstance {
+                index: 81,
+                point: LayoutPoint::new(300.0, 100.0),
+            },
+            GlyphInstance {
+                index: 3,
+                point: LayoutPoint::new(350.0, 100.0),
+            },
+            GlyphInstance {
+                index: 86,
+                point: LayoutPoint::new(400.0, 100.0),
+            },
+            GlyphInstance {
+                index: 79,
+                point: LayoutPoint::new(450.0, 100.0),
+            },
+            GlyphInstance {
+                index: 72,
+                point: LayoutPoint::new(500.0, 100.0),
+            },
+            GlyphInstance {
+                index: 83,
+                point: LayoutPoint::new(550.0, 100.0),
+            },
+            GlyphInstance {
+                index: 87,
+                point: LayoutPoint::new(600.0, 100.0),
+            },
+            GlyphInstance {
+                index: 17,
+                point: LayoutPoint::new(650.0, 100.0),
+            },
+        ];
+
+        builder.push_text(
+            &CommonItemProperties::new(text_bounds, frame_builder.space_and_clip),
+            text_bounds,
+            &glyphs,
+            font,
+            ColorF::new(1.0, 1.0, 0.0, 1.0),
+            None,
+        );
+
+        frame_builder.builder.pop_stacking_context();
+
+        window.render_frame(frame_builder);
+        timer.wait();
+    }
+}
--- src/window.rs
+++ src/window.rs
@@ -3,12 +3,10 @@ use std::fs::File;
 use std::io::Read;
 
 use gleam::gl;
-use glutin::NotCurrent;
+use glutin::{NotCurrent, PossiblyCurrent};
 use webrender::api::units::*;
 use webrender::api::*;
-use webrender::DebugFlags;
 use winit::dpi::{PhysicalPosition, PhysicalSize};
-use winit::platform::run_return::EventLoopExtRunReturn;
 
 struct Notifier {
     events_proxy: winit::event_loop::EventLoopProxy<()>,
@@ -87,7 +85,7 @@ pub struct Window {
     pub document_id: DocumentId,
     epoch: Epoch,
     pub api: RenderApi,
-    pub font_instance_key_hashmap: HashMap<&'static str, FontInstanceKey>,
+    pub font_key_hashmap: HashMap<&'static str, FontKey>,
 }
 
 impl Window {
@@ -125,6 +123,7 @@ impl Window {
             })
             .with_vsync(true)
             .with_double_buffer(Some(true))
+            .with_multisampling(4)
             .build_windowed(window_builder, &events_loop)
             .unwrap();
 
@@ -167,72 +166,47 @@ impl Window {
             pipeline_id,
             document_id,
             api,
-            font_instance_key_hashmap: HashMap::new(),
+            font_key_hashmap: HashMap::new(),
         }
     }
 
-    pub fn load_font_file(&mut self, name: &'static str, pathname: &str, font_size: Au) {
+    pub fn load_font_file(&mut self, name: &'static str, pathname: &str) {
         let mut txn = Transaction::new();
 
         let font_key = self.api.generate_font_key();
         txn.add_raw_font(font_key, load_file(pathname), 0);
 
+        self.font_key_hashmap.insert(name, font_key);
+        self.api.send_transaction(self.document_id, txn);
+    }
+
+    pub fn load_font(&mut self, name: &'static str, font_size: Au) -> FontInstanceKey {
+        let mut txn = Transaction::new();
+
         let font_instance_key = self.api.generate_font_instance_key();
         txn.add_font_instance(
             font_instance_key,
-            font_key,
+            self.font_key_hashmap[&name],
             font_size,
             None,
             None,
             Vec::new(),
         );
-        self.font_instance_key_hashmap
-            .insert(name, font_instance_key);
+
+        self.api.send_transaction(self.document_id, txn);
+
+        font_instance_key
+    }
+
+    pub fn unload_font(&mut self, font_instance_key: FontInstanceKey) {
+        let mut txn = Transaction::new();
+
+        txn.delete_font_instance(font_instance_key);
 
         self.api.send_transaction(self.document_id, txn);
     }
 
-    pub fn tick(&mut self) -> bool {
-        let mut do_exit = false;
-        let my_name = &self.name;
-        let renderer = &mut self.renderer;
-        let api = &mut self.api;
-
-        self.events_loop
-            .run_return(|global_event, _elwt, control_flow| {
-                *control_flow = winit::event_loop::ControlFlow::Exit;
-                match global_event {
-                    winit::event::Event::WindowEvent { event, .. } => match event {
-                        winit::event::WindowEvent::CloseRequested
-                        | winit::event::WindowEvent::KeyboardInput {
-                            input:
-                                winit::event::KeyboardInput {
-                                    virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        } => do_exit = true,
-                        winit::event::WindowEvent::KeyboardInput {
-                            input:
-                                winit::event::KeyboardInput {
-                                    state: winit::event::ElementState::Pressed,
-                                    virtual_keycode: Some(winit::event::VirtualKeyCode::P),
-                                    ..
-                                },
-                            ..
-                        } => {
-                            println!("set flags {}", my_name);
-                            api.send_debug_cmd(DebugCommand::SetFlags(DebugFlags::PROFILER_DBG))
-                        }
-                        _ => {}
-                    },
-                    _ => {}
-                }
-            });
-        if do_exit {
-            return true;
-        }
-
+    pub fn build_frame(&mut self) -> FrameBuilder {
         let context = unsafe { self.context.take().unwrap().make_current().unwrap() };
         let device_pixel_ratio = context.window().scale_factor() as f32;
         let device_size = {
@@ -240,104 +214,42 @@ impl Window {
             DeviceIntSize::new(size.width as i32, size.height as i32)
         };
         let layout_size = device_size.to_f32() / euclid::Scale::new(device_pixel_ratio);
-        let mut txn = Transaction::new();
-        let mut builder = DisplayListBuilder::new(self.pipeline_id, layout_size);
+        let txn = Transaction::new();
+        let builder = DisplayListBuilder::new(self.pipeline_id, layout_size);
         let space_and_clip = SpaceAndClipInfo::root_scroll(self.pipeline_id);
 
         let bounds = LayoutRect::from_size(layout_size);
-        builder.push_simple_stacking_context(
-            bounds.min(),
-            space_and_clip.spatial_id,
-            PrimitiveFlags::IS_BACKFACE_VISIBLE,
-        );
 
-        builder.push_rect(
-            &CommonItemProperties::new(
-                LayoutRect::new(
-                    LayoutPoint::new(100.0, 200.0),
-                    LayoutSize::new(100.0, 200.0),
-                ),
-                space_and_clip,
-            ),
-            ColorF::new(0.0, 1.0, 0.0, 1.0),
-        );
+        FrameBuilder {
+            context,
+            device_size,
+            layout_size,
+            txn,
+            builder,
+            space_and_clip,
+            bounds,
+        }
+    }
 
-        let text_bounds =
-            LayoutRect::new(LayoutPoint::new(100.0, 50.0), LayoutSize::new(700.0, 200.0));
-        let glyphs = vec![
-            GlyphInstance {
-                index: 48,
-                point: LayoutPoint::new(100.0, 100.0),
-            },
-            GlyphInstance {
-                index: 68,
-                point: LayoutPoint::new(150.0, 100.0),
-            },
-            GlyphInstance {
-                index: 80,
-                point: LayoutPoint::new(200.0, 100.0),
-            },
-            GlyphInstance {
-                index: 82,
-                point: LayoutPoint::new(250.0, 100.0),
-            },
-            GlyphInstance {
-                index: 81,
-                point: LayoutPoint::new(300.0, 100.0),
-            },
-            GlyphInstance {
-                index: 3,
-                point: LayoutPoint::new(350.0, 100.0),
-            },
-            GlyphInstance {
-                index: 86,
-                point: LayoutPoint::new(400.0, 100.0),
-            },
-            GlyphInstance {
-                index: 79,
-                point: LayoutPoint::new(450.0, 100.0),
-            },
-            GlyphInstance {
-                index: 72,
-                point: LayoutPoint::new(500.0, 100.0),
-            },
-            GlyphInstance {
-                index: 83,
-                point: LayoutPoint::new(550.0, 100.0),
-            },
-            GlyphInstance {
-                index: 87,
-                point: LayoutPoint::new(600.0, 100.0),
-            },
-            GlyphInstance {
-                index: 17,
-                point: LayoutPoint::new(650.0, 100.0),
-            },
-        ];
-
-        builder.push_text(
-            &CommonItemProperties::new(text_bounds, space_and_clip),
-            text_bounds,
-            &glyphs,
-            self.font_instance_key_hashmap[&"OpenSans"],
-            ColorF::new(1.0, 1.0, 0.0, 1.0),
+    pub fn render_frame(&mut self, mut frame_builder: FrameBuilder) {
+        frame_builder.txn.set_display_list(
+            self.epoch,
             None,
+            frame_builder.layout_size,
+            frame_builder.builder.finalize(),
+            true,
         );
+        frame_builder.txn.set_root_pipeline(self.pipeline_id);
+        frame_builder.txn.generate_frame();
 
-        builder.pop_stacking_context();
+        self.api
+            .send_transaction(self.document_id, frame_builder.txn);
+        self.renderer.update();
+        self.renderer.render(frame_builder.device_size).unwrap();
 
-        txn.set_display_list(self.epoch, None, layout_size, builder.finalize(), true);
-        txn.set_root_pipeline(self.pipeline_id);
-        txn.generate_frame();
-        api.send_transaction(self.document_id, txn);
+        frame_builder.context.swap_buffers().ok();
 
-        renderer.update();
-        renderer.render(device_size).unwrap();
-        context.swap_buffers().ok();
-
-        self.context = Some(unsafe { context.make_not_current().unwrap() });
-
-        false
+        self.context = Some(unsafe { frame_builder.context.make_not_current().unwrap() });
     }
 
     fn load_icon(pathname: &'static str) -> Option<winit::window::Icon> {
@@ -357,6 +269,16 @@ impl Window {
     pub fn deinit(self) {
         self.renderer.deinit();
     }
+}
+
+pub struct FrameBuilder {
+    context: glutin::WindowedContext<PossiblyCurrent>,
+    pub device_size: webrender::euclid::Size2D<i32, units::DevicePixel>,
+    layout_size: webrender::euclid::Size2D<f32, units::LayoutPixel>,
+    pub txn: Transaction,
+    pub builder: DisplayListBuilder,
+    pub space_and_clip: SpaceAndClipInfo,
+    pub bounds: LayoutRect,
 }
 
 fn load_file(name: &str) -> Vec<u8> {
